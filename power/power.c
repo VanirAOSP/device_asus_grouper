76e35e8e220418219d968e22835bb274806b1796
Dennis Rassmann <showp1984@gmail.com>
2012-10-04 17:13:50 -0700

    power: remove cpufreq resets (3/4)
    Change-Id: I6b2330352898cede7d5040023d47bb5f3a52ff57
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- power/power.c
+++ power/power.c
@@ -71,14 +71,6 @@ static void grouper_power_init(struct power_module *module)
 
 static void grouper_power_set_interactive(struct power_module *module, int on)
 {
-    /*
-     * Lower maximum frequency when screen is off.  CPU 0 and 1 share a
-     * cpufreq policy.
-     */
-
-    sysfs_write("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-                on ? "1300000" : "700000");
-
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/input_boost",
                 on ? "1" : "0");
 
