dd2488d10ef6d26e77cf9a8279620708538ce25f
PrimeDirective <activethrasher00@gmail.com>
2013-01-19 16:32:59 -0500

    Revert "Make grouper's powerhal as OC-friendly as tuna's"
    This reverts commit f43b30b9423ca05379af3e8416c8e41ed2add5e7.
    
    Change-Id: I3a156468e3b51b9ede8f85f3f16ed1fa0e4225c7

--- power/power.c
+++ power/power.c
@@ -18,9 +18,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 
 #define LOG_TAG "Grouper PowerHAL"
 #include <utils/Log.h>
@@ -28,24 +25,13 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-#define SCALINGMAXFREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
-#define SCREENOFFMAXFREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/screen_off_max_freq"
 #define BOOST_PATH      "/sys/devices/system/cpu/cpufreq/interactive/boost"
-
-#define MAX_BUF_SZ 10
-
-/* initialize to something safe */
-static char screen_off_max_freq[MAX_BUF_SZ] = "700000";
-static char scaling_max_freq[MAX_BUF_SZ] = "1300000";
-static char buf_screen_off_max[MAX_BUF_SZ], buf_scaling_max[MAX_BUF_SZ];
-static char buf[80];
-static int previous_state = 1;
-
 static int boost_fd = -1;
 static int boost_warned;
 
-static void sysfs_write(char *path, char *s, int l)
+static void sysfs_write(char *path, char *s)
 {
+    char buf[80];
     int len;
     int fd = open(path, O_WRONLY);
 
@@ -64,23 +50,6 @@ static void sysfs_write(char *path, char *s, int l)
     close(fd);
 }
 
-int sysfs_read(const char *path, char *buf, size_t size)
-{
-    int fd, len;
-
-    fd = open(path, O_RDONLY);
-    if (fd < 0)
-            return -1;
-
-    do {
-            len = read(fd, buf, size);
-    } while (len < 0 && errno == EINTR);
-
-    close(fd);
-
-    return len;
-}
-
 static void grouper_power_init(struct power_module *module)
 {
     /*
@@ -89,59 +58,32 @@ static void grouper_power_init(struct power_module *module)
      */
 
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate",
-                "20000", 6);
+                "20000");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time",
-                "30000", 6);
+                "30000");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load",
-                "85", 3);
+                "85");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/boost_factor",
-		"0", 2);
+		"0");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/input_boost",
-		"1", 2);
+		"1");
 }
 
 static void grouper_power_set_interactive(struct power_module *module, int on)
 {
-    memset(buf_screen_off_max, 0, MAX_BUF_SZ);
-    memset(buf_scaling_max, 0, MAX_BUF_SZ);
-    int screen_off_max=0, scaling_max=0;
+    /*
+     * Lower maximum frequency when screen is off.  CPU 0 and 1 share a
+     * cpufreq policy.
+     */
 
-    //screen state has changed since last call
-    if (on != previous_state)
-    {
-        previous_state = on;
-
-        //read value of screen-off max from sysfs, and convert to int for comparison
-        if (sysfs_read(SCREENOFFMAXFREQ_PATH, buf_screen_off_max, sizeof(buf_screen_off_max)) != -1)
-            screen_off_max = atoi(buf_screen_off_max);
-
-        //read value of max from sysfs, and convert to int for comparison
-        if (sysfs_read(SCALINGMAXFREQ_PATH, buf_scaling_max, sizeof(buf_scaling_max)) != -1)
-            scaling_max = atoi(buf_scaling_max);
-
-        //the largest of scaling_max and screen_off_max is truly the value of the CPU maximum frequency
-        //  ... so write it where it belongs.
-        if (scaling_max > screen_off_max)
-            memcpy(scaling_max_freq, 
-               (scaling_max > screen_off_max) ? buf_scaling_max : buf_screen_off_max,
-               strlen((scaling_max > screen_off_max) ? buf_scaling_max : buf_screen_off_max));
-
-        int towrite = strlen((scaling_max <= screen_off_max && screen_off_max > 0) ? buf_scaling_max : buf_screen_off_max);
-
-        //if screen_off_max_freq is 0, use scaling_max_freq in its place
-        memcpy(screen_off_max_freq, 
-               (scaling_max <= screen_off_max && screen_off_max > 0) ? buf_scaling_max : buf_screen_off_max,
-               towrite);
-
-        //write the values back... except for SCREENOFFMAXFREQ_PATH, because we didn't change it.
-        sysfs_write(SCALINGMAXFREQ_PATH, on?scaling_max_freq:screen_off_max_freq, towrite);
-    }
+    sysfs_write("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
+                on ? "1300000" : "700000");
 
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/input_boost",
-                on ? "1" : "0",2);
+                on ? "1" : "0");
 
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/boost_factor",
-                on ? "0" : "2",2);
+                on ? "0" : "2");
 
 }
 
