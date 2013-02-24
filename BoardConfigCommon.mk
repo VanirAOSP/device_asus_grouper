4d6a50d369bae3c56bdd25896327898a1ebcfd5a
PrimeDirective <activethrasher00@gmail.com>
2013-02-24 03:07:54 -0500

    remove graphite experimental flags: not used
    Change-Id: I2ea48da607925c6be8cf00454eb4e98795c24505

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,7 +36,6 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 ARCH_ARM_USE_NON_NEON_MEMCPY := true
-TARGET_USE_GRAPHITE := true
 TARGET_EXTRA_CFLAGS += $(call cc-option,-mtune=cortex-a9) $(call cc-option,-mcpu=cortex-a9)
 
 # Set to true for platforms with 32 byte L2 cache line.
